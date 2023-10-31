9471882637004697c1b228be6d7026f372d9b678
Puripurna0 <146051287+Puripurna0@users.noreply.github.com>
2023-10-31 13:13:59 +0800

    Kuliah Algo 31 oktober 2023

---
+++ 01-programjavapertama/src/App.java
@@ -0,0 +1,35 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Program Biodata Mahasiswa
+        // deklarasi variable
+        String nama;
+        String nim;
+        String tgl_lahir;
+        String alamat;
+        Float berat_badan;
+        Float tinggi_badan;
+    
+    // mengisi variable
+    nama = "Puri_purna";
+    nim = "23241071";
+    tgl_lahir = "09 September 2004";
+    alamat = "Suradadi,Terara, lombok timur";
+    berat_badan = 40f;
+    tinggi_badan = 160f;
+
+    // Cetak Biodata
+    System.out.print1n("====================");
+    System.out.print1n("Nama : " + nama);
+    System.out.println("nim : " + nim);
+    System.out.println("tanggal lahir : " + tgl_lahir);
+    System.out.println(" alamat : " + alamat);
+    System.out.println(" berat badan : " + berat_badan);
+    System.out.println(" tinggi badan : " + tinggi_badan);
+    System.out.println("======================");
+    
+
+
+    
+
+}
+}
